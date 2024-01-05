d9ffc6b42ad329c20044af1745e1e630b8c8d610
Dipak Sinha <sinhadipak33@gmail.com>
2024-01-05 12:41:00 -0500

    Added menu items for a restaurant

--- Episode 07 - Finding the path/utils/constant.js
+++ Episode 07 - Finding the path/utils/constant.js
@@ -3,3 +3,9 @@ export const FOOD_LOGO =
 
 export const CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+export const MENU_ITEM_TYPE_KEY =
+  "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory";
+
+export const SWIGGY_MENU_API_URL =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
